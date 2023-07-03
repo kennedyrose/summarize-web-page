a32ef2c1183d65555e50a1d4352a67977c42cb6f
Kennedy Rose <kennedyianrose@gmail.com>
2023-07-03 16:29:12 -0500

    Summarizer

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import cheerio from 'cheerio'
+import unfluff from 'unfluff'
+import fetch from 'isomorphic-fetch'
+import openai from './open-ai-client'
+
+const defaultOptions = {
+	inputType: `url`,
+	outputType: `array`,
+	prompt: `Summarize the following content in bullet points. Each bullet point should be on its own line should not have any hyphen or number at the start.`,
+	model: `gpt-3.5-turbo`,
+}
+
+export default async function summarize(input, userOptions){
+	const options = {
+		...defaultOptions,
+		...userOptions,
+	}
+	let url
+	let html = options.inputType === `html` ? input : ``
+	let text = options.inputType === `text` ? input : ``
+	let output = {
+		summary: ``,
+		tokens: 0,
+	}
+
+	// Fetch HTML
+	if(options.inputType === `url`){
+		output.url = input
+		const response = await fetch(input)
+		html = await response.text()
+	}
+
+	// Extract text from HTML
+	if(html){
+		const $ = cheerio.load(html)
+		text = unfluff(html).text
+		output.title = $(`title`).text()
+		output.description = $(`meta[name="description"]`).attr(`content`)
+	}
+
+	// Summarize text
+	if(text){
+		const res = await articleToSummary(text, options)
+		output.summary = res.summary
+		output.tokens += res.tokens
+	}
+
+	// Return summary
+	if(options.outputType === `array`){
+		output.summary = output.summary.split(`\n`)
+	}
+	return {
+		summary: finalOutput,
+		meta,
+	}
+
+}
+
+
+
+async function articleToSummary(article, options){
+	// Split article into paragraphs
+	article = article.trim()
+	const lines = article.split(`\n`)
+
+	// Split paragraphs into chunks no greater than inputCharacters
+	const chunks = []
+	let chunk = ``
+	for(let line of lines){
+		if(chunk.length + line.length > inputCharacters){
+			chunks.push(chunk)
+			chunk = ``
+		}
+		chunk += `${line}\n`
+	}
+	chunks.push(chunk)
+
+	// Summarize each chunk
+	const summaries = await Promise.all(chunks.map(chunk => summarizeText(chunk, options)))
+
+	// Join chunks
+	const result = []
+	let tokens = 0
+	for(let i = 0; i < summaries.length; i++){
+		result.push(summaries[i].summary)
+		tokens += summaries[i].tokens
+	}
+
+   const summary = result.join(`\n`)
+	return {
+		summary,
+		tokens,
+	}
+}
+
+async function summarizeText(article, options){
+	const response = await openai.createChatCompletion({
+		model: options.model,
+		messages: [
+			{role: `system`, content: options.prompt},
+			{role: `user`, content: article},
+		],
+	})
+	const tokens = get(response, `data.usage.total_tokens`)
+	const message = get(response, `data.choices[0].message.content`, ``).trim()
+
+	let summary = message.split(`\n`)
+	summary = summary.map(line => {
+		// Remove hyphens from start of line
+		if(line.startsWith(`- `)){
+			line = line.slice(2)
+		}
+		return `- ${line.trim()}`
+	})
+	summary = summary.join(`\n`)
+	return {
+		summary,
+		tokens,
+	}
+}
